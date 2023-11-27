635cb080da92ebfa88ccc3a88db569eec9585e33
Notnobcoder <andresolut@gmail.com>
2023-11-27 14:33:33 +0000

    added c file and standford c cheatsheet for c programin

--- index.cpp
+++ index.cpp
@@ -3,18 +3,19 @@ using namespace std;
 
 
 int main() {
-  // cout << "hello world" << endl;
-  // insergig value in cout and it prints those values
-  // -----variables
-  int a = 6; // every integer needs 4 bytes of data
-  string b = "helo alsjdflkasjdfkljasdfjlkdjsflkjsdf lakdsjflkj world this is my name and this is the value "; // 24 byte size of string
-  bool c = 34; // 1 byte for boolean
-  // int value = a + c;
-  char value = 'H'; // character shourl always be in a single brackets
-  char ch = '#' ; // 1 byte for string
-  cout << sizeof(ch) << " is the size" << endl;
-  // explicit type conversion
-  cout << (int)value << " is the value" << endl;
+  // // cout << "hello world" << endl;
+  // // insergig value in cout and it prints those values
+  // // -----variables
+  // int a = 6; // every integer needs 4 bytes of data
+  // string b = "helo alsjdflkasjdfkljasdfjlkdjsflkjsdf lakdsjflkj world this is my name and this is the value "; // 24 byte size of string
+  // bool c = 34; // 1 byte for boolean
+  // // int value = a + c;
+  // char value = 'H'; // character shourl always be in a single brackets
+  // char ch = '#' ; // 1 byte for string
+  // cout << sizeof(ch) << " is the size" << endl;
+  // // explicit type conversion
+  // cout << (int)value << " is the value" << endl;
+  std::cout << "hello world " << "from c++ language";
 
 
 }
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main () {
+  int x = 42;
+  int y = 42;
+  printf("hello world in c language");
+}
